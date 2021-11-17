23ea4f9f4ec8a440af3f7eb60055d8998c665d95
Mikhail <mmvsha73@gmail.com>
2021-11-17 11:28:56 +0100

    Losing TS battle for now...
    Sent from my iPhone

--- apps/wallet-connect/src/hooks/useWalletConnect.tsx
+++ apps/wallet-connect/src/hooks/useWalletConnect.tsx
@@ -1,11 +1,15 @@
 import { useState, useCallback, useEffect } from 'react';
+import { parseUri } from '@walletconnect/utils';
+import WalletConnectV1 from '@walletconnect/client-v1';
 import WalletConnect from '@walletconnect/client';
-import { IClientMeta } from '@walletconnect/types';
+import { AppMetadata } from '@walletconnect/types';
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
 import { isMetaTxArray } from '../utils/transactions';
 import { areStringsEqual } from '../utils/strings';
 
 export const LOCAL_STORAGE_URI_KEY = 'safeAppWcUri';
+
+type WalletConnectConnector = WalletConnect | WalletConnectV1;
 
 const rejectWithMessage = (connector: WalletConnect, id: number | undefined, message: string) => {
   connector.rejectRequest({ id, error: { message } });
@@ -13,8 +17,8 @@ const rejectWithMessage = (connector: WalletConnect, id: number | undefined, mes
 
 const useWalletConnect = () => {
   const { safe, sdk } = useSafeAppsSDK();
-  const [wcClientData, setWcClientData] = useState<IClientMeta | null>(null);
-  const [connector, setConnector] = useState<WalletConnect | undefined>();
+  const [wcClientData, setWcClientData] = useState<AppMetadata | undefined>();
+  const [connector, setConnector] = useState<WalletConnect | WalletConnectV1 | undefined>();
 
   const wcDisconnect = useCallback(async () => {
     connector?.killSession();
@@ -25,113 +29,117 @@ const useWalletConnect = () => {
 
   const wcConnect = useCallback(
     async (uri: string) => {
-      const wcConnector = new WalletConnect({ uri });
-      setConnector(wcConnector);
-      setWcClientData(wcConnector.peerMeta);
-      localStorage.setItem(LOCAL_STORAGE_URI_KEY, uri);
+      const { version } = parseUri(uri);
 
-      wcConnector.on('session_request', (error, payload) => {
-        if (error) {
-          throw error;
-        }
+      if (version === '1') {
+        const wcConnector = new WalletConnectV1({ uri });
+        setConnector(wcConnector);
+        setWcClientData(wcConnector.peerMeta);
+        localStorage.setItem(LOCAL_STORAGE_URI_KEY, uri);
 
-        wcConnector.approveSession({
-          accounts: [safe.safeAddress],
-          chainId: safe.chainId,
-        });
-
-        setWcClientData(payload.params[0].peerMeta);
-      });
-
-      wcConnector.on('call_request', async (error, payload) => {
-        if (error) {
-          throw error;
-        }
-
-        try {
-          let result = '0x';
-
-          switch (payload.method) {
-            case 'eth_sendTransaction': {
-              const txInfo = payload.params[0];
-              const { safeTxHash } = await sdk.txs.send({
-                txs: [
-                  {
-                    to: txInfo.to,
-                    value: txInfo.value || '0x0',
-                    data: txInfo.data || '0x',
-                  },
-                ],
-              });
-
-              result = safeTxHash;
-              break;
-            }
-            case 'gs_multi_send': {
-              const txs = payload.params;
-
-              if (!isMetaTxArray(txs)) {
-                throw new Error('INVALID_TRANSACTIONS_PROVIDED');
-              }
-
-              const { safeTxHash } = await sdk.txs.send({
-                txs: txs.map((txInfo) => ({
-                  to: txInfo.to,
-                  value: (txInfo.value || '0x0').toString(),
-                  data: txInfo.data || '0x',
-                })),
-              });
-
-              result = safeTxHash;
-              break;
-            }
-
-            case 'personal_sign': {
-              const [message, address] = payload.params;
-
-              if (!areStringsEqual(address, safe.safeAddress)) {
-                throw new Error('The address or message hash is invalid');
-              }
-
-              await sdk.txs.signMessage(message);
-
-              result = '0x';
-              break;
-            }
-
-            case 'eth_sign': {
-              const [address, messageHash] = payload.params;
-
-              if (!areStringsEqual(address, safe.safeAddress) || !messageHash.startsWith('0x')) {
-                throw new Error('The address or message hash is invalid');
-              }
-
-              await sdk.txs.signMessage(messageHash);
-
-              result = '0x';
-              break;
-            }
-            default: {
-              rejectWithMessage(wcConnector, payload.id, 'METHOD_NOT_SUPPORTED');
-              break;
-            }
+        wcConnector.on('session_request', (error, payload) => {
+          if (error) {
+            throw error;
           }
 
-          wcConnector.approveRequest({
-            id: payload.id,
-            result,
+          wcConnector.approveSession({
+            accounts: [safe.safeAddress],
+            chainId: safe.chainId,
           });
-        } catch (err) {
-          rejectWithMessage(wcConnector, payload.id, (err as Error).message);
-        }
-      });
 
-      wcConnector.on('disconnect', (error, payload) => {
-        if (error) {
-          throw error;
-        }
-        wcDisconnect();
-      });
+          setWcClientData(payload.params[0].peerMeta);
+        });
+
+        wcConnector.on('call_request', async (error, payload) => {
+          if (error) {
+            throw error;
+          }
+
+          try {
+            let result = '0x';
+
+            switch (payload.method) {
+              case 'eth_sendTransaction': {
+                const txInfo = payload.params[0];
+                const { safeTxHash } = await sdk.txs.send({
+                  txs: [
+                    {
+                      to: txInfo.to,
+                      value: txInfo.value || '0x0',
+                      data: txInfo.data || '0x',
+                    },
+                  ],
+                });
+
+                result = safeTxHash;
+                break;
+              }
+              case 'gs_multi_send': {
+                const txs = payload.params;
+
+                if (!isMetaTxArray(txs)) {
+                  throw new Error('INVALID_TRANSACTIONS_PROVIDED');
+                }
+
+                const { safeTxHash } = await sdk.txs.send({
+                  txs: txs.map((txInfo) => ({
+                    to: txInfo.to,
+                    value: (txInfo.value || '0x0').toString(),
+                    data: txInfo.data || '0x',
+                  })),
+                });
+
+                result = safeTxHash;
+                break;
+              }
+
+              case 'personal_sign': {
+                const [message, address] = payload.params;
+
+                if (!areStringsEqual(address, safe.safeAddress)) {
+                  throw new Error('The address or message hash is invalid');
+                }
+
+                await sdk.txs.signMessage(message);
+
+                result = '0x';
+                break;
+              }
+
+              case 'eth_sign': {
+                const [address, messageHash] = payload.params;
+
+                if (!areStringsEqual(address, safe.safeAddress) || !messageHash.startsWith('0x')) {
+                  throw new Error('The address or message hash is invalid');
+                }
+
+                await sdk.txs.signMessage(messageHash);
+
+                result = '0x';
+                break;
+              }
+              default: {
+                rejectWithMessage(wcConnector, payload.id, 'METHOD_NOT_SUPPORTED');
+                break;
+              }
+            }
+
+            wcConnector.approveRequest({
+              id: payload.id,
+              result,
+            });
+          } catch (err) {
+            rejectWithMessage(wcConnector, payload.id, (err as Error).message);
+          }
+        });
+
+        wcConnector.on('disconnect', (error, payload) => {
+          if (error) {
+            throw error;
+          }
+          wcDisconnect();
+        });
+      }
     },
     [safe, sdk, wcDisconnect],
   );
